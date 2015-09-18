4f19ab2545ae5ffb6ec418a18feb0046dd53cfb0
JunehwanPark <parkarisma@naver.com>
2015-09-18 21:45:15 +0900

    last

--- button/src/main/java/com/example/com/myapplication/MainActivity.java
+++ button/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.com.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,25 +12,32 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button) findViewById(R.id.button1);
-
-
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요", Toast.LENGTH_SHORT).show();
-                            }
-
-
-        });
     }
+            public void onClick(View v)
+            {
+                switch (v.getId())
+                {
+                    case R.id.btn1 : startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nate.com")));
+
+                        break;
+                    case R.id.btn2 : startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:911")));
+
+                        break;
+
+                    case R.id.btn3 : startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media")));
+
+                        break;
+
+                    case R.id.btn4 : finish();
+
+                        break;
+                }
+            }
+        }
 
 
-}
+
