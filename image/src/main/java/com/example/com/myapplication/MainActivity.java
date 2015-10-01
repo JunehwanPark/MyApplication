bdca974253501495b33b88674ae407216ec50a2f
JunehwanPark <parkarisma@naver.com>
2015-10-02 01:39:09 +0900

    22

--- image/src/main/java/com/example/com/myapplication/MainActivity.java
+++ image/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -16,12 +16,11 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
     TextView text1, text2;
-    Switch chkAgree;
+    Switch switchAgree;
     RadioGroup rGroup1;
-    RadioButton rdoDog, rdoCat, rdoRabbit;
+    RadioButton rdo43, rdo44, rdo50;
     Button btnQuit;
     Button btnFirst;
-
     ImageView imgPet;
 
     @Override
@@ -31,22 +30,22 @@ public class MainActivity extends Activity {
         setTitle("안드로이드 사진 보기");
 
         text1 = (TextView) findViewById(R.id.Text1);
-        chkAgree = (Switch) findViewById(R.id.ChkAgree);
+        switchAgree = (Switch) findViewById(R.id.switchAgree);
 
         text2 = (TextView) findViewById(R.id.Text2);
         rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
-        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
-        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
-        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+        rdo43 = (RadioButton) findViewById(R.id.Rdo43);
+        rdo44 = (RadioButton) findViewById(R.id.Rdo44);
+        rdo50 = (RadioButton) findViewById(R.id.Rdo50);
 
         btnQuit = (Button) findViewById(R.id.BtnQuit);
         btnFirst = (Button) findViewById(R.id.BtnFirst);
         imgPet = (ImageView) findViewById(R.id.ImgPet);
 
-        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switchAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (chkAgree.isChecked() == true) {
+                if (switchAgree.isChecked()) {
                     text2.setVisibility(android.view.View.VISIBLE);
                     rGroup1.setVisibility(android.view.View.VISIBLE);
                     btnFirst.setVisibility(android.view.View.VISIBLE);
@@ -62,21 +61,41 @@ public class MainActivity extends Activity {
             }
         });
 
-        rGroup1.findViewById(R.id.Rgroup1).setOnClickListener(new View.OnClickListener() {
+        rGroup1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (rGroup1.getCheckedRadioButtonId()) {
-                    case R.id.RdoDog:
+                    case R.id.Rdo43:
                         imgPet.setImageResource(R.drawable.dog);
                         break;
-                    case R.id.RdoCat:
+                    case R.id.Rdo44:
                         imgPet.setImageResource(R.drawable.cat);
                         break;
-                    case R.id.RdoRabbit:
+                    case R.id.Rdo50:
                         imgPet.setImageResource(R.drawable.r);
                         break;
                 }
             }
+        });
+
+        btnQuit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
+
+        btnFirst.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                text2.setVisibility(android.view.View.INVISIBLE);
+                rGroup1.setVisibility(android.view.View.INVISIBLE);
+                btnFirst.setVisibility(android.view.View.INVISIBLE);
+                btnQuit.setVisibility(android.view.View.INVISIBLE);
+                imgPet.setVisibility(android.view.View.INVISIBLE);
+
+                rGroup1.clearCheck();
+                switchAgree.setChecked(false);
+            }
+
         });
     }
 }
